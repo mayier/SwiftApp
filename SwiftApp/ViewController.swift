02aee9f7bb8ac4bf6ab8e99697942750629a6809
Megan Yates <mayier7613@gmail.com>
2017-03-19 06:42:29 -0400

    Basic calculator

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -11,15 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
-    
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonPressed(_ sender: Any) {
-        
-        tapCount = tapCount + 1
-        if (tapCount >= 10) {
-            theLabel.text = "You tapped the button 10 times!"
-        }
+        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
     }
     
     override func viewDidLoad() {
